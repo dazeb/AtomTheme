8264cc9ed1c3ddcf6b42e1af0beaf828cbd03fb7
Darren Bennett <dbennett2911@gmail.com>
2023-07-01 11:40:05 +0100

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,5 +38,5 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
   ],
 
-  plugins: ["~/plugins/Vue3Lottie.client.ts", "~/plugins/vercel.js"],
+  plugins: ["~/plugins/Vue3Lottie.client.ts"],
 });
