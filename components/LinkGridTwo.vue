4a7860052d693fb2fd33f6c760a11a8a5a71d339
Darren Bennett <dbennett2911@gmail.com>
2023-07-02 09:22:02 +0100

    added extensions, started gradient theme

---
+++ components/LinkGridTwo.vue
@@ -0,0 +1,150 @@
+<template>
+    <section>
+        <div class="grid max-w-screen-xl grid-cols-1 gap-6 p-8 px-4 mx-auto md:grid-cols-2 lg:grid-cols-3">
+            <article
+                class="rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:shadow-sm">
+                <div class="rounded-[10px] bg-white p-4 sm:p-6">
+                    <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                        <a class="" href="/services/digital-campaigns">
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-blue-500" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                        <path
+                            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+                    </svg> -->
+                            <Icon name="💎" size="24" />
+                            <h2 class="mt-4 text-xl font-bold text-gray-900 dark:text-gray-300">Digital campaigns</h2>
+
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
+                                possimus adipisci distinctio alias voluptatum blanditiis laudantium.
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </article>
+            <article
+                class="rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:shadow-sm">
+                <div class="rounded-[10px] bg-white p-4 sm:p-6">
+                    <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                        <a class="" href="/services/digital-campaigns">
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-blue-500" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                        <path
+                            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+                    </svg> -->
+                            <Icon name="💎" size="24" />
+                            <h2 class="mt-4 text-xl font-bold text-gray-900 dark:text-gray-300">Digital campaigns</h2>
+
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
+                                possimus adipisci distinctio alias voluptatum blanditiis laudantium.
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </article>
+            <article
+                class="rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:shadow-sm">
+                <div class="rounded-[10px] bg-white p-4 sm:p-6">
+                    <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                        <a class="" href="/services/digital-campaigns">
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-blue-500" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                        <path
+                            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+                    </svg> -->
+                            <Icon name="💎" size="24" />
+                            <h2 class="mt-4 text-xl font-bold text-gray-900 dark:text-gray-300">Digital campaigns</h2>
+
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
+                                possimus adipisci distinctio alias voluptatum blanditiis laudantium.
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </article>
+            <article
+                class="rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:shadow-sm">
+                <div class="rounded-[10px] bg-white p-4 sm:p-6">
+                    <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                        <a class="" href="/services/digital-campaigns">
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-blue-500" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                        <path
+                            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+                    </svg> -->
+                            <Icon name="💎" size="24" />
+                            <h2 class="mt-4 text-xl font-bold text-gray-900 dark:text-gray-300">Digital campaigns</h2>
+
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
+                                possimus adipisci distinctio alias voluptatum blanditiis laudantium.
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </article>
+            <article
+                class="rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:shadow-sm">
+                <div class="rounded-[10px] bg-white p-4 sm:p-6">
+                    <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                        <a class="" href="/services/digital-campaigns">
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-blue-500" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                        <path
+                            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+                    </svg> -->
+                            <Icon name="💎" size="24" />
+                            <h2 class="mt-4 text-xl font-bold text-gray-900 dark:text-gray-300">Digital campaigns</h2>
+
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
+                                possimus adipisci distinctio alias voluptatum blanditiis laudantium.
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </article>
+            <article
+                class="rounded-xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 p-0.5 shadow-xl transition hover:shadow-sm">
+                <div class="rounded-[10px] bg-white p-4 sm:p-6">
+                    <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                        <a class="" href="/services/digital-campaigns">
+                            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-blue-500" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                        <path
+                            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+                    </svg> -->
+                            <Icon name="💎" size="24" />
+                            <h2 class="mt-4 text-xl font-bold text-gray-900 dark:text-gray-300">Digital campaigns</h2>
+
+                            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo
+                                possimus adipisci distinctio alias voluptatum blanditiis laudantium.
+                            </p>
+                        </a>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </section>
+</template>
---
+++ pages/npage.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+// compiler macro
+definePageMeta({
+    layout: 'default',
+})
+useHead(() => ({
+    title: 'page',
+}))
+</script>
+
+<template>
+    <PageWrapper>
+        <PageHeader>
+            <PageTitle text="page" class="capitalize" />
+        </PageHeader>
+        <PageBody>
+            <PageSection>
+                <div>PAGE_BODY</div>
+            </PageSection>
+        </PageBody>
+    </PageWrapper>
+</template>
